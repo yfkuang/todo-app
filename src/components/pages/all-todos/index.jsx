22baee505b38f07db01a56594c5de3da32145fdb
Ye Fang Kuang <yefang@opsesports.ca>
2021-10-15 22:51:59 -0400

    Mapped todos

--- src/components/pages/all-todos/index.jsx
+++ src/components/pages/all-todos/index.jsx
@@ -2,12 +2,30 @@ import "./styles.css"
 import { Todo } from "../../todo/index"
 
 export const AllTodosPage = () => {
+    const todos = [
+        {
+            text: "Walk my dog",
+            date: "10/10/2021",
+            color: "blue",
+            isComplete: false,
+        },
+        {
+            text: "asdasd",
+            date: "10/10/2021",
+            color: "blue",
+            isComplete: false,
+        },
+        {
+            text: "asdasdasd",
+            date: "10/10/2021",
+            color: "blue",
+            isComplete: true,
+        },
+    ]
+    
     return (
-        <div>
-            <Todo
-                text="Walk my dog"
-                date="10/10/2021"
-                color="blue"></Todo>
+        <div className="todos-container">
+            { todos.map((todo) => <Todo text={todo.text} date={todo.date} color={todo.color} isComplete={todo.isComplete}/>) }
         </div>
     )
 }
